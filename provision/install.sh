c9d161b9b1704838e13f06284c46dec4ee131ae2
Daniel Dreier <ddreier@thinkplango.com>
2014-02-16 11:02:15 -0800

    Remove unnecessary files
    Former-commit-id: 842f21b54f852d4057409d1a15e6e0789f010173

--- provision/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hash puppet && exit 0
-hash serf && exit 0
-
-yum -y install http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
-yum -y install puppet
--- provision/install_serf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-hash serf && exit 0
-iptables -F # don't put this into production...
-cp /vagrant/serf/serf /usr/bin/
-hash serf || echo "serf not in path"

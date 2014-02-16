0e8301e124a88c51baf1a0f0b8586bca1a57e50a
Daniel Dreier <ddreier@thinkplango.com>
2014-02-16 14:35:37 -0800

    Updated readme, comments in site.pp
    Former-commit-id: 731fbbeb9ab29f4baf2e889de2d708170cdaea50

--- deploy/site.pp
+++ deploy/site.pp
@@ -1,5 +1,7 @@
 node default {
   class { 'serf':
+    # Note that this is using a hacked version of the serf module to allow
+    # setting the "advertise" parameter. Submitted a pull request.
     version       => '0.4.1',
     bind          => $::ipaddress_eth1,
     advertise     => $::ipaddress_eth1,

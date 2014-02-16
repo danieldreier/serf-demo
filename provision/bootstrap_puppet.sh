842f21b54f852d4057409d1a15e6e0789f010173
Daniel Dreier <ddreier@thinkplango.com>
2014-02-16 11:02:15 -0800

    Remove unnecessary files

--- provision/bootstrap_puppet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p /etc/puppet
-cp /opt/puppet/puppet.conf /etc/puppet/puppet.conf
-puppet apply -vv /vagrant/puppet/manifests/bootstrap.pp
--- puppet/manifests/serf.pp
+++
@@ -1,5 +0,0 @@
-class { 'serf':
-  version => '0.4.1',
-  bind    => $::ipaddress_eth1,
-}
-
